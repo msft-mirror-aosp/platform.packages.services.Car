041bbf8174ac11c53b24ffdb59d85fc00e95a6cb
Changyeon Jo <changyeon@google.com>
2021-12-10 12:29:04 -0800

    Does not add the evs manager by default
    Bug: 204163764
    Test: Build and confirm android.automotive.evs.manager@1.1.xml does not
          exist in /system/etc/vintf/ manually.
    Change-Id: Iee8971072dc536c0b255e707e6bfa635a1a16f21

--- car_product/build/car_base.mk
+++ car_product/build/car_base.mk
@@ -65,8 +65,8 @@ PRODUCT_PACKAGES += \
     car-bugreportd \
 
 # EVS resources
-PRODUCT_PACKAGES += android.automotive.evs.manager@1.1
 # The following packages, or their vendor specific equivalents should be include in the device.mk
+#PRODUCT_PACKAGES += android.automotive.evs.manager@1.1
 #PRODUCT_PACKAGES += evs_app
 #PRODUCT_PACKAGES += evs_app_default_resources
 #PRODUCT_PACKAGES += android.hardware.automotive.evs@1.0-service
