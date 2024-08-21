fc3963aa510d20e5ef20cafa1bdd8ea8a7e68069
Samuel Johnson <samueldjohnson@google.com>
2024-08-21 12:42:50 -0700

    Replace package uid time with total process time if it is larger
    Sometimes, during a PerformanceProfiler dump,
    process cpu time is larger than package uid time.
    This replaces package uid time with total process
    time if total process time is larger.
    
    Test: atest libwatchdog_test
    Flag: EXEMPT refactor
    Fixes: 259613585
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:7a09755fe12608331f54b18cc0b01067abbc145d)
    Merged-In: Ifa08c191810353a00e07afd4022fef8afc46b204
    
    Change-Id: Ifa08c191810353a00e07afd4022fef8afc46b204

--- cpp/watchdog/server/src/PerformanceProfiler.cpp
+++ cpp/watchdog/server/src/PerformanceProfiler.cpp
@@ -219,17 +219,26 @@ std::string UserPackageStats::toString(MetricType metricsType,
 
 std::string UserPackageStats::toString(int64_t totalValue) const {
     std::string buffer;
+    int64_t totalProcessTimeMs = 0;
     auto procCpuStatsView = std::get_if<UserPackageStats::ProcCpuStatsView>(&statsView);
     if (procCpuStatsView != nullptr) {
+        // Sometimes, the CPU time used by a specific process (reported in /proc/<pid>/stat) is
+        // higher than the total CPU time used by all processes belonging to the same UID (reported
+        // in /proc/uid_cputime/show_uid_stat). This can happen if the UID only has one process
+        // running. The reason is likely a difference in how the kernel calculates and reports CPU
+        // usage in these two places. To ensure accuracy, use the higher of the two reported total
+        // CPU times when calculating percentage.
+        for (const auto& processCpuValue : procCpuStatsView->topNProcesses) {
+            totalProcessTimeMs += processCpuValue.cpuTimeMs;
+        }
+        totalProcessTimeMs = std::max(totalProcessTimeMs, procCpuStatsView->cpuTimeMs);
         StringAppendF(&buffer, "%" PRIu32 ", %s, %" PRIu64 ", %.2f%%, %" PRIu64 "\n",
-                      multiuser_get_user_id(uid), genericPackageName.c_str(),
-                      procCpuStatsView->cpuTimeMs,
-                      percentage(procCpuStatsView->cpuTimeMs, totalValue),
-                      procCpuStatsView->cpuCycles);
+                      multiuser_get_user_id(uid), genericPackageName.c_str(), totalProcessTimeMs,
+                      percentage(totalProcessTimeMs, totalValue), procCpuStatsView->cpuCycles);
         for (const auto& processCpuValue : procCpuStatsView->topNProcesses) {
             StringAppendF(&buffer, "\t%s, %" PRIu64 ", %.2f%%, %" PRIu64 "\n",
                           processCpuValue.comm.c_str(), processCpuValue.cpuTimeMs,
-                          percentage(processCpuValue.cpuTimeMs, procCpuStatsView->cpuTimeMs),
+                          percentage(processCpuValue.cpuTimeMs, totalProcessTimeMs),
                           processCpuValue.cpuCycles);
         }
         return buffer;
