355abcae8999d39ad3f486edafddd9397029daca
Yu Shan <shanyu@google.com>
2024-10-08 13:44:11 -0700

    Add safety check for value not being null.
    Add requireNonNull check for the value argument passed to
    CarPropertyValue. This is to make sure we do not accidentally pass
    in null.
    
    Flag: EXEMPT minor change
    Test: Presubmit
    Bug: 372298480
    Change-Id: Idced91fc0fcde512f10fd779a30f16b62504d133

--- tests/carservice_unit_test/src/com/android/car/CarPropertyServiceUnitTest.java
+++ tests/carservice_unit_test/src/com/android/car/CarPropertyServiceUnitTest.java
@@ -495,17 +495,6 @@ public final class CarPropertyServiceUnitTest {
     }
 
     @Test
-    public void testSetPropertiesAsync_nullValueToSet() {
-        AsyncPropertyServiceRequest request = new AsyncPropertyServiceRequest(
-                0, READ_WRITE_INT_PROPERTY_ID, 0, new CarPropertyValue(READ_WRITE_INT_PROPERTY_ID,
-                        0, /* value= */ null));
-
-        assertThrows(IllegalArgumentException.class, () -> mService.setPropertiesAsync(
-                new AsyncPropertyServiceRequestList(List.of(request)),
-                mAsyncPropertyResultCallback, ASYNC_TIMEOUT_MS));
-    }
-
-    @Test
     public void testSetPropertiesAsync_mismatchPropertyType() {
         AsyncPropertyServiceRequest request = new AsyncPropertyServiceRequest(
                 0, READ_WRITE_INT_PROPERTY_ID, 0, new CarPropertyValue(READ_WRITE_INT_PROPERTY_ID,
@@ -1364,13 +1353,6 @@ public final class CarPropertyServiceUnitTest {
         assertThrows(IllegalArgumentException.class, () -> mService.setProperty(
                 new CarPropertyValue(ON_CHANGE_READ_WRITE_PROPERTY_ID, NOT_SUPPORTED_AREA_ID,
                         Integer.MAX_VALUE), mICarPropertyEventListener));
-    }
-
-    @Test
-    public void setProperty_throwsExceptionBecauseOfNullSetValue() {
-        assertThrows(IllegalArgumentException.class, () -> mService.setProperty(
-                new CarPropertyValue(ON_CHANGE_READ_WRITE_PROPERTY_ID, GLOBAL_AREA_ID, null),
-                mICarPropertyEventListener));
     }
 
     @Test
