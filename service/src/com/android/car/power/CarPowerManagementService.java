8c3ae70c173b1d108e9ce76c80f64805f35c51cf
Maxim Pleshivenkov <mpleshivenkov@google.com>
2024-11-11 19:04:39 +0000

    Update power policy notification mechanism
    The CPMS could drop policy change notifications if a new request
     was received while a previous one was still being processed.
    This was due to the policy update data being overwritten by the new request.
    
    To resolve that, necessary data is included with the request to notify subscribers.
    
    Bug: 367742695
    Flag: EXEMPT minor bugfix
    Test: atest CarPowerManagementServiceUnitTest
    Change-Id: I4af1ec72407f3dae9566709517d3bbeefba06330
    (cherry picked from commit bba0bd42a10eac24d66e83f601836340919dbc94)

--- service/src/com/android/car/power/CarPowerManagementService.java
+++ service/src/com/android/car/power/CarPowerManagementService.java
@@ -86,6 +86,7 @@ import android.os.UserManager;
 import android.util.ArraySet;
 import android.util.AtomicFile;
 import android.util.SparseArray;
+import android.util.SparseBooleanArray;
 import android.util.proto.ProtoOutputStream;
 import android.view.Display;
 
@@ -975,7 +976,9 @@ public class CarPowerManagementService extends ICarPower.Stub implements
         }
         CarPowerPolicy accumulatedPolicy = request.getAccumulatedPolicy();
         updateCurrentPowerPolicy(accumulatedPolicy);
-        notifyPowerPolicyChange(accumulatedPolicy);
+        notifyPowerPolicyChange(new PowerPolicyChangeNotification(
+                mPowerComponentHandler.getLastModifiedComponents(),
+                accumulatedPolicy, /* legacyNotification= */ false));
     }
 
     private void handleWaitForVhal(CpmsState state) {
@@ -1896,13 +1899,13 @@ public class CarPowerManagementService extends ICarPower.Stub implements
         mScreenOffHandler.handleDisplayStateChange(displayId, on);
     }
 
-    private void doHandlePowerPolicyNotification(String policyId) {
+    private void doHandlePowerPolicyNotification(PowerPolicyChangeNotification notification) {
         // Sending notification of power policy change triggered through CarPowerManager API.
-        notifyPowerPolicyChange(policyId, /* upToDaemon= */ true, /* force= */ false);
-    }
-
-    private void doHandlePowerPolicyNotification(CarPowerPolicy accumulatedPolicy) {
-        notifyPowerPolicyChange(accumulatedPolicy);
+        if (notification.legacyNotification) {
+            notifyPowerPolicyChange(notification, /* upToDaemon= */ true, /* force= */ false);
+        } else {
+            notifyPowerPolicyChange(notification);
+        }
     }
 
     /**
@@ -2320,7 +2323,9 @@ public class CarPowerManagementService extends ICarPower.Stub implements
             updateCurrentPowerPolicy(currentAccumulatedPolicy);
             String policyId = accumulatedPolicy.policyId;
             Slogf.i(TAG, "Queueing power policy notification (ID: %s) in the handler", policyId);
-            mHandler.handlePowerPolicyNotification(currentAccumulatedPolicy);
+            mHandler.handlePowerPolicyNotification(new PowerPolicyChangeNotification(
+                    mPowerComponentHandler.getLastModifiedComponents(),
+                    currentAccumulatedPolicy, /* legacyNotification= */ false));
         }
 
         @Override
@@ -2396,7 +2401,9 @@ public class CarPowerManagementService extends ICarPower.Stub implements
                     powerPolicyInitData.currentPowerPolicy);
             updateCurrentPowerPolicy(currentPowerPolicy);
             mPowerComponentHandler.applyPowerPolicy(currentPowerPolicy);
-            notifyPowerPolicyChange(currentPowerPolicy);
+            notifyPowerPolicyChange(new PowerPolicyChangeNotification(
+                    mPowerComponentHandler.getLastModifiedComponents(),
+                    currentPowerPolicy, /* legacyNotification= */ false));
             // To cover the case where power state changed before connecting to CPPD.
             int currentPowerState = getPowerState();
             if (currentPowerState != CarPowerManager.STATE_WAIT_FOR_VHAL
@@ -2655,12 +2662,15 @@ public class CarPowerManagementService extends ICarPower.Stub implements
             }
             CarPowerPolicy accumulatedPolicy = request.getAccumulatedPolicy();
             updateCurrentPowerPolicy(accumulatedPolicy);
+            PowerPolicyChangeNotification notification = new PowerPolicyChangeNotification(policyId,
+                    mPowerComponentHandler.getLastModifiedComponents(),
+                    mPowerComponentHandler.getAccumulatedPolicy(), /* legacyNotification= */ false);
             if (delayNotification) {
                 Slogf.d(TAG,
                         "Queueing power policy notification (id: %s) in the handler", policyId);
-                mHandler.handlePowerPolicyNotification(accumulatedPolicy);
+                mHandler.handlePowerPolicyNotification(notification);
             } else {
-                notifyPowerPolicyChange(accumulatedPolicy);
+                notifyPowerPolicyChange(notification);
             }
             Trace.traceEnd(TraceHelper.TRACE_TAG_CAR_SERVICE);
         } else {
@@ -2680,12 +2690,15 @@ public class CarPowerManagementService extends ICarPower.Stub implements
                 mCurrentPowerPolicyId = policyId;
             }
             mPowerComponentHandler.applyPowerPolicy(policy);
+            PowerPolicyChangeNotification notification = new PowerPolicyChangeNotification(
+                    mPowerComponentHandler.getLastModifiedComponents(),
+                    policy, /* legacyNotification= */ true);
             if (delayNotification) {
                 Slogf.d(TAG,
                         "Queueing power policy notification (id: %s) in the handler", policyId);
-                mHandler.handlePowerPolicyNotification(policyId);
+                mHandler.handlePowerPolicyNotification(notification);
             } else {
-                notifyPowerPolicyChange(policyId, upToDaemon, force);
+                notifyPowerPolicyChange(notification, upToDaemon, force);
             }
         }
         Slogf.i(TAG, "The current power policy is %s", policyId);
@@ -2708,7 +2721,11 @@ public class CarPowerManagementService extends ICarPower.Stub implements
             mCurrentPowerPolicyId = policyId;
         }
         mPowerComponentHandler.applyPowerPolicy(policy);
-        notifyPowerPolicyChange(policyId, /* upToDaemon= */ true, /* force= */ true);
+        notifyPowerPolicyChange(new PowerPolicyChangeNotification(policyId,
+                        mPowerComponentHandler.getLastModifiedComponents(),
+                        mPowerComponentHandler.getAccumulatedPolicy(),
+                        /* legacyNotification= */ true),
+                /* upToDaemon= */ true, /* force= */ true);
         Slogf.i(TAG, "The current power policy is %s", policyId);
         return PolicyOperationStatus.OK;
     }
@@ -2761,8 +2778,10 @@ public class CarPowerManagementService extends ICarPower.Stub implements
 
     // TODO(b/286303350): remove after power policy refactor is complete - will only use version
     //                    that takes 'accumulatedPowerPolicy' as input
-    private void notifyPowerPolicyChange(String policyId, boolean upToDaemon, boolean force) {
+    private void notifyPowerPolicyChange(PowerPolicyChangeNotification policyChangeNotification,
+            boolean upToDaemon, boolean force) {
         // Notify system clients
+        String policyId = policyChangeNotification.policyId;
         if (upToDaemon) {
             notifyPowerPolicyChangeToDaemon(policyId, force);
         }
@@ -2771,7 +2790,7 @@ public class CarPowerManagementService extends ICarPower.Stub implements
                 ? mPolicyReader.getPreemptivePowerPolicy(policyId)
                 : mPolicyReader.getPowerPolicy(policyId);
         CarPowerPolicy accumulatedPolicy = mPowerComponentHandler.getAccumulatedPolicy();
-        notifyPowerPolicyChange(policyId, appliedPolicy, accumulatedPolicy);
+        notifyPowerPolicyChangeToListeners(policyChangeNotification, appliedPolicy);
     }
 
     @Nullable
@@ -2789,8 +2808,8 @@ public class CarPowerManagementService extends ICarPower.Stub implements
         return mPolicyReader.getPowerPolicy(policyId);
     }
 
-    private void notifyPowerPolicyChange(CarPowerPolicy accumulatedPowerPolicy) {
-        String policyId = accumulatedPowerPolicy.getPolicyId();
+    private void notifyPowerPolicyChange(PowerPolicyChangeNotification policyChangeNotification) {
+        String policyId = policyChangeNotification.policyId;
         try {
             CarPowerPolicy appliedPolicy =
                     getPowerPolicyDefinition(policyId, CAR_POWER_POLICY_DEFINITION_TIMEOUT_MS);
@@ -2798,18 +2817,20 @@ public class CarPowerManagementService extends ICarPower.Stub implements
                 Slogf.wtf(TAG, "The new power policy(%s) should exist", policyId);
                 return;
             }
-            notifyPowerPolicyChange(policyId, appliedPolicy, accumulatedPowerPolicy);
+            notifyPowerPolicyChangeToListeners(policyChangeNotification, appliedPolicy);
         } catch (InterruptedException e) {
             Slogf.e(TAG, e, "Failed to get power policy definition for policy ID %s", policyId);
         }
     }
 
-    // TODO(b/286303350): after power policy refactor is complete, remove this function and replace
-    //                    the inner call to it by notifyPowerPolicyChange(accumulatedPowerPolicy)
-    //                    with this function's code contents
-    private void notifyPowerPolicyChange(String policyId, CarPowerPolicy appliedPolicy,
-            CarPowerPolicy accumulatedPolicy) {
+    private void notifyPowerPolicyChangeToListeners(
+            PowerPolicyChangeNotification policyChangeNotification, CarPowerPolicy appliedPolicy) {
+        String policyId = policyChangeNotification.policyId;
+
+        SparseBooleanArray updatedComponents = policyChangeNotification.lastModifiedComponents;
+
         EventLogHelper.writePowerPolicyChange(policyId);
+
         if (appliedPolicy == null) {
             Slogf.wtf(TAG, "The new power policy(%s) should exist", policyId);
         }
@@ -2822,11 +2843,12 @@ public class CarPowerManagementService extends ICarPower.Stub implements
                 ICarPowerPolicyListener listener = mPowerPolicyListeners.getBroadcastItem(idx);
                 CarPowerPolicyFilter filter =
                         (CarPowerPolicyFilter) mPowerPolicyListeners.getBroadcastCookie(idx);
-                if (!mPowerComponentHandler.isComponentChanged(filter)) {
+                if (!PowerComponentHandler.isComponentChanged(updatedComponents, filter)) {
                     continue;
                 }
                 try {
-                    listener.onPolicyChanged(appliedPolicy, accumulatedPolicy);
+                    listener.onPolicyChanged(appliedPolicy,
+                            policyChangeNotification.accumulatedPolicy);
                 } catch (RemoteException e) {
                     // It's likely the connection snapped. Let binder death handle the situation.
                     Slogf.e(TAG, e, "onPolicyChanged() call failed: policyId = %s", policyId);
@@ -3092,13 +3114,9 @@ public class CarPowerManagementService extends ICarPower.Stub implements
             removeMessages(MSG_PROCESSING_COMPLETE);
         }
 
-        private void handlePowerPolicyNotification(String policyId) {
-            Message msg = obtainMessage(MSG_POWER_POLICY_NOTIFICATION, policyId);
-            sendMessage(msg);
-        }
 
-        private void handlePowerPolicyNotification(CarPowerPolicy accumulatedPolicy) {
-            Message msg = obtainMessage(MSG_POWER_POLICY_NOTIFICATION, accumulatedPolicy);
+        private void handlePowerPolicyNotification(PowerPolicyChangeNotification notification) {
+            Message msg = obtainMessage(MSG_POWER_POLICY_NOTIFICATION, notification);
             sendMessage(msg);
         }
 
@@ -3134,11 +3152,8 @@ public class CarPowerManagementService extends ICarPower.Stub implements
                     service.doHandleProcessingComplete();
                     break;
                 case MSG_POWER_POLICY_NOTIFICATION:
-                    if (mFeatureFlags.carPowerPolicyRefactoring()) {
-                        service.doHandlePowerPolicyNotification((CarPowerPolicy) msg.obj);
-                    } else {
-                        service.doHandlePowerPolicyNotification((String) msg.obj);
-                    }
+                    service.doHandlePowerPolicyNotification(
+                            (PowerPolicyChangeNotification) msg.obj);
                     break;
                 default:
                     Slogf.w(TAG, "handleMessage invalid message type: %d", msg.what);
@@ -4065,5 +4080,17 @@ public class CarPowerManagementService extends ICarPower.Stub implements
     @VisibleForTesting
     void changeShouldChangeSwap(boolean value) {
         mShouldChangeSwap = value;
+    }
+
+    // TODO (b/286303350): remove legacyNotification field after power policy refactor is complete
+    private record PowerPolicyChangeNotification(String policyId,
+                                                 SparseBooleanArray lastModifiedComponents,
+                                                 CarPowerPolicy accumulatedPolicy,
+                                                 boolean legacyNotification) {
+        PowerPolicyChangeNotification(SparseBooleanArray lastModifiedComponents,
+                CarPowerPolicy accumulatedPolicy, boolean legacyNotification) {
+            this(accumulatedPolicy.getPolicyId(), lastModifiedComponents, accumulatedPolicy,
+                    legacyNotification);
+        }
     }
 }
