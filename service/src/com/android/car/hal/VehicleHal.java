07d83b9b6f7e4c5bd259d9d6db5dbd96b22fd86d
Yu Shan <shanyu@google.com>
2024-11-22 12:11:55 -0800

    Implement SupportedValues in AidlVehicleStub
    Flag: android.car.feature.car_property_supported_value
    Test: atest AidlVehicleStubUnitTest VehicleHalTest
    Bug: 371636116
    
    Change-Id: I1a61064ace26be1d9679d30fc1af8cf9714edd17

--- service/src/com/android/car/hal/VehicleHal.java
+++ service/src/com/android/car/hal/VehicleHal.java
@@ -64,7 +64,6 @@ import com.android.car.CarSystemService;
 import com.android.car.VehicleStub;
 import com.android.car.VehicleStub.MinMaxSupportedRawPropValues;
 import com.android.car.VehicleStub.SubscriptionClient;
-import com.android.car.VehicleStub.SupportedValuesChangeCallback;
 import com.android.car.internal.ExcludeFromCodeCoverageGeneratedReport;
 import com.android.car.internal.common.DispatchList;
 import com.android.car.internal.property.PropIdAreaId;
@@ -92,8 +91,7 @@ import java.util.concurrent.TimeUnit;
  * implementation. It is the responsibility of {@link HalServiceBase} to convert data to
  * corresponding Car*Service for Car*Manager API.
  */
-public class VehicleHal implements VehicleHalCallback, CarSystemService,
-        SupportedValuesChangeCallback {
+public class VehicleHal implements VehicleHalCallback, CarSystemService {
     private static final boolean DBG = Slogf.isLoggable(CarLog.TAG_HAL, Log.DEBUG);
     private static final long TRACE_TAG = TraceHelper.TRACE_TAG_CAR_SERVICE;
 
@@ -308,7 +306,6 @@ public class VehicleHal implements VehicleHalCallback, CarSystemService,
                 mPropertyHal);
         mVehicleStub = vehicle;
         mSubscriptionClient = vehicle.newSubscriptionClient(this);
-        vehicle.setSupportedValuesChangeCallback(this);
     }
 
     /** Sets fake feature flag for unit testing. */
@@ -1875,7 +1872,7 @@ public class VehicleHal implements VehicleHalCallback, CarSystemService,
 
             // Here we do not filter out already registered [propId, areaId]s, we expect each
             // service to filter out duplicate requests.
-            mVehicleStub.registerSupportedValuesChange(propIdAreaIds);
+            mSubscriptionClient.registerSupportedValuesChange(propIdAreaIds);
 
             for (int i = 0; i < propIdAreaIds.size(); i++) {
                 registeredPropIdAreaIds.add(propIdAreaIds.get(i));
@@ -1919,7 +1916,7 @@ public class VehicleHal implements VehicleHalCallback, CarSystemService,
             if (propIdAreaIdsToUnRegister.isEmpty()) {
                 return;
             }
-            mVehicleStub.unregisterSupportedValuesChange(propIdAreaIdsToUnRegister);
+            mSubscriptionClient.unregisterSupportedValuesChange(propIdAreaIdsToUnRegister);
         }
     }
 }
