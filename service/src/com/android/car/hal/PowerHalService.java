3054ba8acf0247c77fca5f74a625d4edc9daf726
Antonio Fuentes <janton29@ford.com>
2025-07-24 18:06:45 -0700

    Fix missing AP_POWER_STATE_REQ events
    if DISPLAY_BRIGHTNESS and AP_POWER_STATE_REQ comes within the same list based on AIDL Vehiclehal interface event,
    then the AP_POWER_STATE_REQ wont be handled due to the return instead of break
    
    Bug: 431211668
    Test: manual testing
    
    Signed-off-by: Antonio Fuentes <janton29@ford.com>
    (cherry picked from https://android-review.googlesource.com/q/commit:a9e9349df13b8cff560e6edf15c85b5c4f3f44e2)
    Merged-In: Iaeca3c08c1c26b8e0501cef05665e913b5114029
    Change-Id: Iaeca3c08c1c26b8e0501cef05665e913b5114029

--- service/src/com/android/car/hal/PowerHalService.java
+++ service/src/com/android/car/hal/PowerHalService.java
@@ -897,7 +897,7 @@ public class PowerHalService extends HalServiceBase {
                         if (mPerDisplayBrightnessSupported) {
                             Slogf.w(CarLog.TAG_POWER, "Received DISPLAY_BRIGHTNESS "
                                     + "while PER_DISPLAY_BRIGHTNESS is supported, ignore");
-                            return;
+                            break;
                         }
                         maxBrightness = mMaxDisplayBrightness;
                     }
@@ -923,7 +923,7 @@ public class PowerHalService extends HalServiceBase {
                         synchronized (mLock) {
                             if (hasRecentlySetBrightnessChangeLocked(brightness,
                                     getDisplayPort(Display.DEFAULT_DISPLAY))) {
-                                return;
+                                break;
                             }
                         }
 
@@ -952,7 +952,7 @@ public class PowerHalService extends HalServiceBase {
                     // caused by that change and is duplicate. Ignore to prevent loop.
                     synchronized (mLock) {
                         if (hasRecentlySetBrightnessChangeLocked(brightness, displayPort)) {
-                            return;
+                            break;
                         }
                     }
 
